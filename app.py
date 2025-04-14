866f3d8202c89fc18e05f4c73bac3c6ba49a8d51
Tuansiiusiudzai <90133765+Tuansiiusiudzai@users.noreply.github.com>
2025-04-14 07:43:33 +0700

    app.py

--- app.py
+++ app.py
@@ -80,6 +80,5 @@ def download():
         return {"error": f"Tải video thất bại: {str(e)}"}, 500
 
 if __name__ == "__main__":
-    # Sửa tại đây để Railway chạy đúng cổng
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
